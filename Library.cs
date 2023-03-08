ca0b7f717ea64cdc934cc5e31622dd1c7a4efbfc
HandrailAltitude <emil.wallman@gmail.com>
2023-03-08 15:04:16 +0100

    Everything

---
+++ Library.cs
@@ -0,0 +1,220 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace library
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public Book(string title, string author, bool isAvailable)
+        {
+            Title = title;
+            Author = author;
+            IsAvailable = isAvailable;
+        }
+
+        public Book(string title, string author)
+        {
+            Title = title;
+            Author = author;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} by {Author} ({(IsAvailable ? "available" : "not available")})";
+        }
+    }
+
+    class Library
+    {
+        private List<Book> books;
+
+        public Library()
+        {
+            books = new List<Book>();
+
+            // Read books from file
+            string[] lines = File.ReadAllLines("books.txt");
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(',');
+                string title = parts[0].Trim();
+                string author = parts[1].Trim();
+                bool isAvailable = bool.Parse(parts[2].Trim());
+                books.Add(new Book(title, author, isAvailable));
+            }
+        }
+
+        public void DisplayBooks()
+        {
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+
+        public void LendBook(string title)
+        {
+            Book book = FindBookByTitle(title);
+            if (book != null && book.IsAvailable)
+            {
+                book.IsAvailable = false;
+                Console.WriteLine($"Successfully lent {book.Title}!");
+            }
+            else if (book != null && !book.IsAvailable)
+            {
+                Console.WriteLine($"{book.Title} is not available for lending.");
+            }
+            else
+            {
+                Console.WriteLine($"Could not find book with title {title}.");
+            }
+        }
+
+        public void ReturnBook(string title)
+        {
+            Book book = FindBookByTitle(title);
+            if (book != null && !book.IsAvailable)
+            {
+                book.IsAvailable = true;
+                Console.WriteLine($"Successfully returned {book.Title}!");
+            }
+            else if (book != null && book.IsAvailable)
+            {
+                Console.WriteLine($"{book.Title} has already been returned.");
+            }
+            else
+            {
+                Console.WriteLine($"Could not find book with title {title}.");
+            }
+        }
+
+        public void ReserveBook(string title)
+        {
+            Book book = FindBookByTitle(title);
+            if (book != null && book.IsAvailable)
+            {
+                Console.WriteLine($"{book.Title} is already available for lending.");
+            }
+            else if (book != null && !book.IsAvailable)
+            {
+                Console.WriteLine($"Successfully reserved {book.Title}!");
+            }
+            else
+            {
+                Console.WriteLine($"Could not find book with title {title}.");
+            }
+        }
+
+        private Book FindBookByTitle(string title)
+        {
+            foreach (Book book in books)
+            {
+                if (book.Title.Equals(title, StringComparison.OrdinalIgnoreCase))
+                {
+                    return book;
+                }
+            }
+            return null;
+        }
+
+        public void AddBook(string title, string author)
+        {
+            Book book = new Book(title, author);
+            books.Add(book);
+            Console.WriteLine($"Added {book} to library.");
+        }
+
+        public void DeleteBook(string title)
+        {
+            Book book = books.FirstOrDefault(b => b.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+            if (book != null)
+            {
+                books.Remove(book);
+                Console.WriteLine($"Removed {book} from library.");
+            }
+            else
+            {
+                Console.WriteLine($"Could not find book with title {title}.");
+            }
+        }
+
+    }
+    //Admin
+    class Admin
+    {
+        public static void AddBook(Library library)
+        {
+            Console.WriteLine("Enter title of book to add:");
+            string title = Console.ReadLine();
+            Console.WriteLine("Enter author of book to add:");
+            string author = Console.ReadLine();
+            library.AddBook(title, author);
+        }
+
+        public static void DeleteBook(Library library)
+        {
+            Console.WriteLine("Enter title of book to delete:");
+            string title = Console.ReadLine();
+            library.DeleteBook(title);
+        }
+    }
+
+
+    class LibraryProgram
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library();
+
+            while (true)
+            {
+                Console.WriteLine("Enter command (display/lend/return/reserve/quit):");
+                string command = Console.ReadLine();
+
+                if (command.Equals("display", StringComparison.OrdinalIgnoreCase))
+                {
+                    library.DisplayBooks();
+                }
+                else if (command.Equals("lend", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Enter title of book to lend:");
+                    string title = Console.ReadLine();
+                    library.LendBook(title);
+                }
+                else if (command.Equals("return", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Enter title of book to return:");
+                    string title = Console.ReadLine();
+                    library.ReturnBook(title);
+                }
+                else if (command.Equals("reserve", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Enter title of book to reserve:");
+                    string title = Console.ReadLine();
+                    library.ReserveBook(title);
+                }
+                else if (command.Equals("quit", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid command.");
+                }
+            }
+
+            Console.WriteLine("Goodbye!");
+        }
+    }
+
+}
+*/
+
---
+++ Program.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+namespace library
+{
+    class User
+    {
+        public string username;
+        public string password;
+        public string ssn;
+        public bool isAdmin;
+
+
+        public User(string username, string password, string ssn, bool isAdmin)
+        {
+            this.username = username;
+            this.password = password;
+            this.isAdmin = isAdmin;
+            this.ssn = ssn;
+        }
+    }
+
+    class LoginSite
+    {
+        private List<User> users;
+        private User currentUser;
+
+        public LoginSite()
+        {
+            users = new List<User>();
+            LoadUsers();
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Welcome to the Login Site");
+
+            while (true)
+            {
+                Console.WriteLine("1. Login");
+                Console.WriteLine("2. Create Account");
+                Console.WriteLine("3. Exit");
+
+                Console.Write("Enter your choice: ");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Login();
+                        break;
+                    case 2:
+                        CreateAccount();
+                        break;
+                    case 3:
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Try again.");
+                        break;
+                }
+            }
+        }
+
+        private void Login()
+        {
+            Console.Write("Enter your username: ");
+            string username = Console.ReadLine();
+            Console.Write("Enter your Social Secutiry Number: ");
+            string ssn = Console.ReadLine();
+            Console.Write("Enter your password: ");
+            string password = "";
+            ConsoleKeyInfo key;
+            do
+            {
+                key = Console.ReadKey(true);
+
+                // Backspace should remove the last character from the password
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                {
+                    password += key.KeyChar;
+                    Console.Write("*");
+                }
+                else if (key.Key == ConsoleKey.Backspace && password.Length > 0)
+                {
+                    password = password.Remove(password.Length - 1);
+                    Console.Write("\b \b");
+                }
+            } while (key.Key != ConsoleKey.Enter);
+
+            Console.WriteLine();
+
+
+            foreach (User user in users)
+            {
+                if (user.username == username && user.password == password && user.ssn == ssn)
+                {
+                    currentUser = user;
+                    Console.WriteLine("Login successful.");
+                    if (user.isAdmin)
+                    {
+                        Console.WriteLine("Welcome, Admin.");
+                        AdminMenu();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Welcome, " + username + ".");
+                        UserMenu();
+                    }
+                    return;
+                }
+            }
+
+            Console.WriteLine("Incorrect username or password. Try again.");
+        }
+
+        private void CreateAccount()
+        {
+            Console.Write("Enter a username: ");
+            string username = Console.ReadLine();
+            Console.Write("Enter your Social Secutiy Number: ");
+            string ssn = Console.ReadLine();
+            Console.Write("Enter a password: ");
+            string password = "";
+            ConsoleKeyInfo key;
+            do
+            {
+                key = Console.ReadKey(true);
+
+                // Backspace should remove the last character from the password
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                {
+                    password += key.KeyChar;
+                    Console.Write("*");
+                }
+                else if (key.Key == ConsoleKey.Backspace && password.Length > 0)
+                {
+                    password = password.Remove(password.Length - 1);
+                    Console.Write("\b \b");
+                }
+            } while (key.Key != ConsoleKey.Enter);
+
+            Console.WriteLine();
+
+
+            // check if a user with the same username or ssn already exists
+            if (GetUserByUsername(username) != null)
+            {
+                Console.WriteLine("Username already exists. Try again.");
+                return;
+            }
+            if (GetUserBySSN(ssn) != null)
+            {
+                Console.WriteLine("Social Security Number already exists. Try again.");
+                return;
+            }
+
+            User user = new User(username, password, ssn, false);
+            users.Add(user);
+            SaveUsers();
+
+            Console.WriteLine("Account created successfully.");
+
+            Thread.Sleep(1000);
+
+            Console.Clear();
+        }
+
+
+        private void AdminMenu()
+        {
+            Console.WriteLine("1. Change User Status");
+            Console.WriteLine("2. Logout");
+            Console.WriteLine("3. Edit library");
+            Console.Write("Enter your choice: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Enter the username: ");
+                    string username = Console.ReadLine();
+                    User user = GetUserByUsername(username);
+                    if (user != null)
+                    {
+                        user.isAdmin = !user.isAdmin;
+                        Console.WriteLine("User status changed successfullyto " + (user.isAdmin ? "Admin" : "Regular") + ".");
+                        SaveUsers();
+                    }
+                    else
+                    {
+                        Console.WriteLine("User not found.");
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Logging out...");
+                    return;
+                case 3:
+
+                default:
+                    Console.WriteLine("Invalid choice. Try again.");
+                    break;
+            }
+
+
+
+            AdminMenu();
+        }
+
+        private void UserMenu()
+        {
+            Console.WriteLine("1. Logout");
+
+            Console.Write("Enter your choice: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Logging out...");
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Try again.");
+                    break;
+            }
+            UserMenu();
+        }
+
+        private User GetUserByUsername(string username)
+        {
+            foreach (User user in users)
+            {
+                if (user.username == username)
+                {
+                    return user;
+                }
+            }
+            return null;
+        }
+
+        private User GetUserBySSN(string ssn)
+        {
+            foreach (User user in users)
+            {
+                if (user.ssn == ssn)
+                {
+                    return user;
+                }
+            }
+            return null;
+        }
+
+
+        private void LoadUsers()
+        {
+            if (!File.Exists("users.txt"))
+            {
+                return;
+            }
+
+            string[] lines = File.ReadAllLines("users.txt");
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(',');
+                string username = parts[0];
+                string password = parts[1];
+                string ssn = parts[2];
+                bool isAdmin = bool.Parse(parts[3]);
+
+                User user = new User(username, password, ssn, isAdmin);
+                users.Add(user);
+            }
+        }
+
+        private void SaveUsers() { 
+        
+            List<string> lines = new List<string>();
+            foreach (User user in users)
+            {
+                lines.Add(user.username + "," + user.password + "," + user.ssn + "," + user.isAdmin);
+            }
+            File.WriteAllLines("users.txt", lines);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoginSite loginSite = new LoginSite();
+            loginSite.Start();
+        }
+    }
+
+}
+
