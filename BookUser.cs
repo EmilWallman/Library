86ae366df9ef51e0935a6984bc0312a2be391993
HandrailAltitude <emil.wallman@gmail.com>
2023-03-14 22:16:45 +0100

    lite skit

---
+++ BookUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace library
+{
+    public class BookUser
+    {
+
+        public int id;
+        public bool inLine;
+        public int ssn;
+        public int placeInLine;
+
+        public BookUser(int id, bool inLine, int ssn, int placeInLine)
+        {
+            this.id = id;
+            this.inLine = inLine;
+            this.ssn = ssn;
+            this.placeInLine = placeInLine;
+        }
+
+    }
+
+}
